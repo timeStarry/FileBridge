4ac2c7d529cd216629c50fe764902ad71b5bd1ad
suoyuan666 <107405806+suoyuan666@users.noreply.github.com>
2024-05-27 12:09:25 +0800

    add main.cpp and some useful files

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <cxxopts.hpp>
+#include <iostream>
+
+auto main(int argc, char* argv[]) -> int {
+    cxxopts::Options options("fb", "基于C++的端到端文件传输服务");
+    options.add_options()
+        ("send", "send file")
+        ("recv", "recive file")
+        ("f,file", "The filename(s) to process",
+            cxxopts::value<std::vector<std::string>>())
+        ("h,help", "help infomation");
+    
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help") != 0U) {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
+
+    if(result.count("send") != 0U){
+        std::cout << "send! \n";
+        return 0;
+    }
+    if(result.count("recv") != 0U){
+        std::cout << "send! \n";
+        return 0;
+    }
+
+    if (result.count("file") != 0U) {
+        auto files = result["file"].as<std::vector<std::string>>();
+        for (const auto& val : files) {
+            std::cout << val << "\n";
+        }
+        return 0;
+    }
+
+    std::cout << options.help() << std::endl;
+    return 0;
+}
